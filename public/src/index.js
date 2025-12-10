dbe23ecaf7a974037a50d9cc730e97ebc6fb6f46
bpexxl-web <bpexxl@gmail.com>
2025-12-10 23:54:11 +0900

    Create index.js

---
+++ public/src/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
+```
+
+**"Commit new file"** 클릭
+
+---
+
+## 📄 4. src/App.js (마지막!)
+
+**이제 가장 중요한 파일입니다!**
+
+**"Add file" → "Create new file"**
+
+### 파일 이름:
+```
+src/App.js
